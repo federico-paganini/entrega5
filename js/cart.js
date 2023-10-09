a98ca40c617c810f3f811c8e88195e7db6a88a75
Agustina Fleitas <agustinaafd@gmail.com>
2023-10-09 16:23:31 -0300

    Ajustes

--- js/cart.js
+++ js/cart.js
@@ -38,5 +38,39 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //Datos de los productos desde "product-info" a la página "cart"
+const URL = PRODUCT_INFO_URL + localStorage.getItem("ValorID") + EXT_TYPE;
+
+//Llamar los datos desde la URL
+fetch (URL)
+.then(res=>{
+    if (!res.ok){
+        throw new error("Hubo un error")
+    }
+    return res.json();
+})
+.then(data =>{
+    console.log('Datos del producto:', data);
+})
+.catch(error => {
+    console.error('Error de solicitud:', error);
+});
+
 const infoProducto=JSON.parse(localStorage.getItem('infoProducto')) || [];
 console.log('Datos del carrito:', infoProducto); //Verificar datos en consola
+
+
+infoProducto.forEach((DatosProducto)=>{
+    const ValorPrecioTotal= DatosProducto.precio * DatosProducto.cantidad;
+    const ProductoDiv= document.createElement("div");
+    
+    ProductoDiv.innerHTML = `
+        <tr class="text-center">
+            <th><img id="imageCart" src="${DatosProducto .imagen}"></th>
+            <td>${DatosProducto .nombre}</td>
+            <td>${DatosProducto .precio}</td>
+            <td><input class="text-center small-input-carrito" value="${DatosProducto .cantidad}" placeholder="${DatosProducto .cantidad}"></td>
+            <td class="negrita">${ValorPrecioTotal}</td>
+            <td><button class="btn btn-danger btn-sm">Eliminar</button></td>
+        </tr>`;
+    productosCarrito.appendChild(ProductoDiv);
+});
