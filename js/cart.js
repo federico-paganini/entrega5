40bf765b7082b2b4f0b77bca2ba6e1fa7bdffe27
Martín Amaral <martin.amaral.g96@gmail.com>
2023-10-09 15:55:47 -0300

    Cambiazo

--- js/cart.js
+++ js/cart.js
@@ -8,27 +8,34 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(cartData => {
             console.log(cartData);
             cartData.articles.forEach(product => {
-                agregarProducto(product);
+                agregarProductoFecheado(product);
             });
         })
         .catch(error => {
             console.error("La solicitud no se completó correctamente", error);
         });
 
-    function agregarProducto(producto) {
+        function agregarProductoFecheado(producto){
+
+            agregarProducto(producto.name,producto.currency,producto.image,producto.unitCost,producto.count)
+
+        }
+
+
+    function agregarProducto(nombre,moneda,imagen,costoUnitario,cantidad) {
 
         let inputcart= document.createElement("input");
-        inputcart.value=producto.count;
+        inputcart.value=cantidad;
         inputcart.placeholder=inputcart.value;
         inputcart.classList.add("text-center","small-input-carrito");
 
         productosCarrito.innerHTML +=`
                 <tr class=text-center>
-                    <th><img id="imageCart" src=${producto.image}></th>
-                    <td>${producto.name}</td>
-                    <td>${producto.currency +" "+ producto.unitCost}</td>
+                    <th><img id="imageCart" src=${imagen}></th>
+                    <td>${nombre}</td>
+                    <td>${moneda +" "+ costoUnitario}</td>
                     <td>${inputcart.outerHTML}</td>
-                    <td class="negrita">${producto.currency +" "+(producto.count * producto.unitCost)}</td>
+                    <td class="negrita">${moneda +" "+(cantidad * costoUnitario)}</td>
                     <td>
                         <button class="btn btn-danger btn-sm">Eliminar</button>
                     </td>
